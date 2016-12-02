d22ebef6ab349f6c54a2734c45f1e4c6e49820a9
wus <swunderlin@gmail.com>
2016-12-02 17:40:06 +0100

    implemented html list

---
+++ config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+config for l-r.com playlist scraper
+
+database schema:
+
+CREATE TABLE playlist(
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   dt INTEGER, -- unix timestamp
+   song TEXT,
+   artist TEXT,
+   album TEXT
+);
+"""
+
+DEBUG = 0 # 0 to disable
+url = "http://www.lounge-radio.com/code/pushed_files/recently.html"
+base_folder = "/tmp"
+db = "./lounge-radio.sqlite3"
+limit = 100
